3bff6c24b5b212fd8a2c1394a62efa609c0630ef
Hugo Norte <hugonorte@gmail.com>
2024-06-17 21:19:30 -0300

    page js

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,13 @@
 import {redirect} from 'next/navigation';
+import {getTranslations} from 'next-intl/server';
+
+export async function generateMetadata({params: {locale}}) {
+  const t = await getTranslations({locale, namespace: 'Metadata'});
+ 
+  return {
+    professional_title: t('professional_title')
+  };
+}
 
 // This page only renders when the app is built statically (output: 'export')
 export default function RootPage() {
